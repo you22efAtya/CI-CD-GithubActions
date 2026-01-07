054c4d7788a59d549da81618a90445daa7468f46
you22efAtya <youssefmohamedyahoo.com9@gmail.com>
2026-01-07 21:59:50 +0200

    real deployment to aws

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -4,6 +4,12 @@ const exec = require('@actions/exec');
 
 function run()
 {
+    const bucket = core.getInput('bucket', { required: true });
+    const bucketRegion = core.getInput('bucket-region', { required: true });
+    const distFolder = core.getInput('dist-folder', { required: true });
+
+    const s3Uri = `s3://${bucket}`;
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
     core.notice('Hello from deploy-s3-javascript action!');
 }
 run();
