1603e38145497a5b8f36de243f70efa60fd95f31
you22efAtya <youssefmohamedyahoo.com9@gmail.com>
2026-01-07 22:06:43 +0200

    output website url

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -10,6 +10,8 @@ function run()
 
     const s3Uri = `s3://${bucket}`;
     exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
-    core.notice('Hello from deploy-s3-javascript action!');
+    
+    const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+    core.setOutput('website-url', websiteUrl);
 }
 run();
